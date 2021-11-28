5792027af7ab2adce47760e27b99ef9886cecbfd
Blazej Sewera <code@sewera.dev>
2021-11-28 14:48:57 +0100

    feat(core): add gorilla websocket chat example

--- core/cmd/rr/main.go
+++ core/cmd/rr/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	s "github.com/jazzsewera/reactive-raven/core/internal/impl"
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
-		time.Sleep(time.Second)
-		fmt.Println("Current:", i)
-	}
-	fmt.Println("Finished")
+	s.Serve()
 }
--- core/go.mod
+++ core/go.mod
@@ -2,7 +2,4 @@ module github.com/jazzsewera/reactive-raven/core
 
 go 1.17
 
-require (
-	github.com/gin-gonic/gin v1.7.2
-	github.com/stretchr/testify v1.7.0
-)
+require github.com/gorilla/websocket v1.4.2
